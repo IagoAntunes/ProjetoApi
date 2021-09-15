9bed79457b4a76da1e21753d8802ceab5412c249
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-15 11:50:37 -0300

    Iago Antunes Ferreira

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,7 +1,15 @@
+using System.Net.Mime;
+using System;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace api.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {}
+
+        public DbSet<Pessoa> pessoa{get;set;}
     }
 }
